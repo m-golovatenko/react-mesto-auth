bded338b0ab93c1c17c37944cab270c0cf0beee7
Masha Golovatenko <pikch4y@yandex.com>
2023-06-23 18:39:51 +0300

    feat: add header appearance according to the path

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,16 +1,33 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
+import { Link, useLocation } from 'react-router-dom';
 
 function Header({ headerInfo, signOut }) {
+  const location = useLocation();
+
   return (
     <header className="header ">
       <Link to="/" className="header__logo" aria-label="Логотип прокта Место" />
-      <div className="header__info">
-        <p className="header__email">{headerInfo}</p>
-        <Link to="" className="header__link" onClick={signOut}>
-          Выйти
+
+      {location.pathname === '/sign-in' && (
+        <Link to="/sign-up" className="header__link">
+          Зарегистрироваться
         </Link>
-      </div>
+      )}
+
+      {location.pathname === '/sign-up' && (
+        <Link to="/sign-in" className="header__link">
+          Войти
+        </Link>
+      )}
+
+      {location.pathname === '/' && (
+        <div className="header__info">
+          <p className="header__email">{headerInfo}</p>
+          <Link to="/sign-in" className="header__link" onClick={signOut}>
+            Выйти
+          </Link>
+        </div>
+      )}
     </header>
   );
 }
